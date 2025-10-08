8228f7d3571cf8c09e122fbd98a8523526e2b6c7
LesbatHaithem <168595728+LesbatHaithem@users.noreply.github.com>
2025-10-08 14:54:02 +0100

    Test and main function added

--- Days.java
+++ Days.java
@@ -19,3 +19,4 @@ private Days(int numJouralgerie, String nomJour) {
 
 
 }
+}
---
+++ Test.java
@@ -0,0 +1,10 @@
+public class Test {
+    public static void main(String[] args) {
+        Days  JourDebutFormation = Days.DIM ;
+        Days JourFinFormation = Days.JEU ;
+        System.out.println(JourDebutFormation.GetnumeJour());
+        System.out.println(JourDebutFormation.GetnomJour());
+        System.out.println(JourFinFormation.GetnumeJour());
+        System.out.println(JourFinFormation.GetnomJour());
+    }
+}
